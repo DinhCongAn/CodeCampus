c4d52ff6ee349e7e9238629ba77b23111527dfe5
Dinh Cong An <andche186895@fpt.edu.vn>
2025-10-10 02:35:15 +0700

    Create project

---
+++ source/CodeCampus/src/test/java/com/codecampus/CodeCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
