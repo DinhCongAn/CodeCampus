dbdf7179a2037a50e525b6a989809a3ffee81fde
Dinh Cong An <andche186895@fpt.edu.vn>
2025-10-10 02:35:15 +0700

    Create project

---
+++ source/CodeCampus/src/main/java/com/codecampus/CodeCampusApplication.java
@@ -0,0 +1,13 @@
+package com.codecampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCampusApplication.class, args);
+    }
+
+}
