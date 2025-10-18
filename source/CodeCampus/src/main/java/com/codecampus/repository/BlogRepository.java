c1b02d66a692d3bf9dafee4e54a59ecc8336e181
Dinh Cong An <andche186895@fpt.edu.vn>
2025-10-19 01:51:50 +0700

    Home

--- source/CodeCampus/src/main/java/com/codecampus/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecampus.repository;
-
-import com.codecampus.entity.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-}
---
+++ source/CodeCampus/src/main/java/com/codecampus/slider/SliderRepository.java
@@ -0,0 +1,11 @@
+package com.codecampus.slider;
+
+import com.codecampus.entity.Slider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SliderRepository extends JpaRepository<Slider, Integer> {
+    // Tìm các slider theo trạng thái
+    List<Slider> findByStatus(String status);
+}
